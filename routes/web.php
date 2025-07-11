e392d4dcee4e05f2bd3b621c04fd84212cfff382
DarlanHenrique <darlan.silva@ice.ufjf.br>
2025-07-11 20:34:32 -0300

    Configure web links

--- routes/web.php
+++ routes/web.php
@@ -2,16 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\Api\LinkController;
+use Illuminate\Http\Request;
+
+
 
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::get('/{short_code}', [LinkController::class, 'redirect']);
+
+
+/* Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
-});
+}); */
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
