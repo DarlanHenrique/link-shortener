2f263249741b72ab33d8768cc976875672b3f519
DarlanHenrique <darlan.silva@ice.ufjf.br>
2025-07-15 11:02:37 -0300

    change web routes

--- routes/web.php
+++ routes/web.php
@@ -5,20 +5,18 @@ use Inertia\Inertia;
 use App\Http\Controllers\Api\LinkController;
 use Illuminate\Http\Request;
 
-
-
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::get('/{short_code}', [LinkController::class, 'redirect']);
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
 
-
-/* Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
-}); */
+});
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+
+Route::get('/{short_code}', [LinkController::class, 'redirect']);
