55b4448ceeb30acfb29c594ad9d9f6ea27dcad9b
DarlanHenrique <darlan.silva@ice.ufjf.br>
2025-07-15 11:03:02 -0300

    changing app sidebar and logo in login

--- resources/js/components/user-info.tsx
+++ resources/js/components/user-info.tsx
@@ -1,6 +1,8 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { useInitials } from '@/hooks/use-initials';
 import { type User } from '@/types';
+import UserIcon from './user-icon';
+
 
 export function UserInfo({ user, showEmail = false }: { user: User; showEmail?: boolean }) {
     const getInitials = useInitials();
@@ -10,7 +12,7 @@ export function UserInfo({ user, showEmail = false }: { user: User; showEmail?: 
             <Avatar className="h-8 w-8 overflow-hidden rounded-full">
                 <AvatarImage src={user.avatar} alt={user.name} />
                 <AvatarFallback className="rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white">
-                    {getInitials(user.name)}
+                    <UserIcon className="size-4" />
                 </AvatarFallback>
             </Avatar>
             <div className="grid flex-1 text-left text-sm leading-tight">
